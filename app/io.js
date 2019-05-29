eaa3cc96442fd0faeaf71c5fcbe2bc5e35e83d86
matzkoh <matzkoh@gmail.com>
2019-05-29 19:35:53 +0900

    change message

--- app/io.js
+++ app/io.js
@@ -1,7 +1,6 @@
-const dedent = require('dedent');
 const sio = require('socket.io');
 const { Score, getScoreList } = require('../entity/score');
-const { postToSlack } = require('./slack');
+const { postScore } = require('./slack');
 
 module.exports = server => {
   const io = sio(server);
@@ -14,13 +13,7 @@ module.exports = server => {
       const score = new Score(data.name, data.time);
       await score.save();
       next();
-      postToSlack({
-        icon_emoji: ':new:',
-        username: 'sisisinout-cube',
-        text: dedent`
-          ${data.name} さんが ${getTimeText(data.time)} でクリアしました
-        `,
-      });
+      postScore(data);
       io.emit('score:list', await getScoreList());
     },
   };
@@ -31,28 +24,3 @@ module.exports = server => {
     });
   });
 };
-
-function getTimeText(time) {
-  if (!time) {
-    return '';
-  }
-  function pad(n, val) {
-    return String(val).padStart(n, '0');
-  }
-  const ms = time % 1000;
-  const seconds = (time / 1000) % 60 | 0;
-  const minutes = (time / 1000 / 60) % 60 | 0;
-  const hours = (time / 1000 / 60 / 60) | 0;
-  let res = '';
-  if (hours) {
-    res += `${pad(2, hours)}時間`;
-  }
-  if (res || minutes) {
-    res += `${pad(2, minutes)}分`;
-  }
-  if (res || seconds) {
-    res += `${pad(2, seconds)}秒`;
-  }
-  res += pad(3, ms);
-  return res;
-}
--- app/slack.js
+++ app/slack.js
@@ -1,7 +1,44 @@
+const dedent = require('dedent');
 const axios = require('axios').default;
 
 async function postToSlack(params) {
   return axios.post(process.env.WEBHOOK_URL, params);
 }
 
-module.exports = { postToSlack };
+async function postScore(score) {
+  const time = getTimeText(score.time);
+  return postToSlack({
+    icon_emoji: ':new:',
+    username: 'sisisinout-cube',
+    text: dedent`
+      ${score.name} さんが ${time} でクリアしました <https://sisisinout-cube.glitch.me/|sisisinout-cube>
+    `,
+  });
+}
+
+function getTimeText(time) {
+  if (!time) {
+    return '';
+  }
+  function pad(n, val) {
+    return String(val).padStart(n, '0');
+  }
+  const ms = time % 1000;
+  const seconds = (time / 1000) % 60 | 0;
+  const minutes = (time / 1000 / 60) % 60 | 0;
+  const hours = (time / 1000 / 60 / 60) | 0;
+  let res = '';
+  if (hours) {
+    res += `${pad(2, hours)}時間`;
+  }
+  if (res || minutes) {
+    res += `${pad(2, minutes)}分`;
+  }
+  if (res || seconds) {
+    res += `${pad(2, seconds)}秒`;
+  }
+  res += pad(3, ms);
+  return res;
+}
+
+module.exports = { postToSlack, postScore };
